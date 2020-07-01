7eafe26868e3b29d4b53ae0b393db560a6b922c0
Bartek Wilczyński <bartekw2213@gmail.com>
2020-07-01 11:44:42 +0200

    displaying expotential number in another way

--- src/Components/Calculator/CalcComponents/Display/Display.js
+++ src/Components/Calculator/CalcComponents/Display/Display.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import './Display.css';
 
-const Display = () => {
+const Display = ({ value }) => {
+  let displayedValue;
+
+  if (value && value.toString().includes('e')) {
+    const valStart = /\d\.\d*/.exec(value);
+    const valEnd = /e.*/.exec(value);
+    displayedValue = (
+      <p>
+        <span>{valStart}</span> <span className='exponentialEnd'>{valEnd}</span>
+      </p>
+    );
+  } else {
+    displayedValue = value;
+  }
+
   return (
     <div className='DisplayContainer'>
-      <div className='Display'></div>
+      <div className='Display'>{displayedValue}</div>
       <div className='DisplayShadow'></div>
       <div className='DisplaySecondShadow'></div>
     </div>
